da8b176d282e9425011769c4e647f17c32aa976d
edycakra <47028300+edycakra@users.noreply.github.com>
2022-07-24 16:30:34 +0700

    success: joining data

--- cleaner_app.sh
+++ cleaner_app.sh
@@ -10,11 +10,16 @@
 # csvgrep -c "event_type" -m "purchase" data/total-raw.csv | csvcut -c event_time,event_type,product_id,category_id,brand,price > data/filtered.csv
 # echo "========== successfully created: data/filtered.csv"
 
-# melakukan splitting data kategori produk dan nama produk pada kolom category_code dengan cara mengambil element pertama dan terakhir dari splitted element
-echo "========== editing data, processing please wait.."
-echo "========== initialize new columns: category, product_name"
-echo $"category, product_name" > data/edited.csv
-echo "========== successfully created: data/edited.csv"
-echo "========== continue splitting data, processing please wait.."
-grep "purchase" data/total-raw.csv | awk -F "," '{print $6}' | awk -F "." '{print $1","$NF}' >> data/edited.csv
-echo "========== successfully splitting data: data/edited.csv"
+# # melakukan splitting data kategori produk dan nama produk pada kolom category_code dengan cara mengambil element pertama dan terakhir dari splitted element
+# echo "========== editing data, processing please wait.."
+# echo "========== initialize new columns: category, product_name"
+# echo $"category, product_name" > data/edited.csv
+# echo "========== successfully created: data/edited.csv"
+# echo "========== continue splitting data, processing please wait.."
+# grep "purchase" data/total-raw.csv | awk -F "," '{print $6}' | awk -F "." '{print $1","$NF}' >> data/edited.csv
+# echo "========== successfully splitting data: data/edited.csv"
+
+# melakukan penggabungan dari filtered dan edited
+echo "========== joining data, processing please wait.."
+csvjoin data/filtered.csv data/edited.csv > data/data_clean.csv
+echo "========== successfully joining data: data/data_clean.csv"
